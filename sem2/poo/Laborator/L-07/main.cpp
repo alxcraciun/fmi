29f33d66ecc1315d53930e8dc375e9164b7d20d3
AlexCraciun <56975249+alxcraciun@users.noreply.github.com>
2023-04-06 23:11:52 +0300

    Update POO

---
+++ sem2/poo/Laborator/L-07/main.cpp
@@ -0,0 +1,24 @@
+/*
+
+C-style cast poate fi orice tip de cast
+
+Static Cast -> same shit ca si cast-ul normal
+
+Dynamic Cast -> forteaza castul si daca nu merge face null 
+
+Reintrepret Cast -> poate schimba valoarea de la adresa aia
+
+Const Cast -> 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
